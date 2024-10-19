576f5b73e169d22b43b5fc5cb4d347923450f211
akkipaul2000 <skypaul2000@gmail.com>
2024-10-19 23:26:43 +0530

    new build changes

--- src/constant.js
+++
@@ -1,7 +0,0 @@
-export const BACKEND_URL = 'https://siwe-backend-39yg.onrender.com';
-
-// export const BACKEND_URL = 'http://localhost:5000';
-
-// export const FRONTEND_URL = 'http://localhost:5173/quiz/'
-
-//  export const FRONTEND_URL = 'https://quizzie-1.vercel.app/quiz/'
